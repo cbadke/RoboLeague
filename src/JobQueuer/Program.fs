01316dde99d5124fbe4a256726f59f76a26098dd
Curtis Badke <curtis.badke@devfacto.com>
2016-01-16 12:39:02 -0700

    Start over from AzureSDK 2.8 cloud service template.

--- src/JobQueuer/Program.fs
+++ src/JobQueuer/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-open Microsoft.ServiceBus
+﻿open Microsoft.ServiceBus
 open Microsoft.ServiceBus.Messaging
 open System.Diagnostics
 
@@ -8,14 +6,14 @@ let log message (kind : string) = Trace.TraceInformation(message, kind)
 
 [<EntryPoint>]
 let main argv = 
-    let HttpPort = 9355
-    let TcpPort = 9354
+    let httpPort = 9355
+    let tcpPort = 9354
     let _serverFqdn = System.Environment.MachineName
-    let ServiceNamespace = "roboleague"
-    let connectionStringBuilder = new ServiceBusConnectionStringBuilder (ManagementPort = HttpPort, RuntimePort = TcpPort)
+    let serviceNamespace = "roboleague"
+    let connectionStringBuilder = new ServiceBusConnectionStringBuilder (ManagementPort = httpPort, RuntimePort = tcpPort)
 
-    connectionStringBuilder.Endpoints.Add((new System.UriBuilder (Scheme = "sb", Host = _serverFqdn, Path = ServiceNamespace)).Uri) |> ignore
-    connectionStringBuilder.StsEndpoints.Add((new System.UriBuilder (Scheme = "https", Host = _serverFqdn, Port = HttpPort, Path = ServiceNamespace)).Uri) |> ignore
+    connectionStringBuilder.Endpoints.Add((new System.UriBuilder (Scheme = "sb", Host = _serverFqdn, Path = serviceNamespace)).Uri) |> ignore
+    connectionStringBuilder.StsEndpoints.Add((new System.UriBuilder (Scheme = "https", Host = _serverFqdn, Port = httpPort, Path = serviceNamespace)).Uri) |> ignore
 
     let queueName = "games"
     let messageFactory = MessagingFactory.CreateFromConnectionString(connectionStringBuilder.ToString());
--- src/RunsGames/WorkerRole.fs
+++ src/RunsGames/WorkerRole.fs
@@ -6,9 +6,8 @@ open System.Diagnostics
 open System.Linq
 open System.Net
 open System.Threading
-open Microsoft.ServiceBus
-open Microsoft.ServiceBus.Messaging
 open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Diagnostics
 open Microsoft.WindowsAzure.ServiceRuntime
 
 type WorkerRole() =
@@ -17,34 +16,13 @@ type WorkerRole() =
     // This is a sample worker implementation. Replace with your logic.
 
     let log message (kind : string) = Trace.TraceInformation(message, kind)
-    let HttpPort = 9355
-    let TcpPort = 9354
-    let _serverFqdn = System.Environment.MachineName
-    let ServiceNamespace = "roboleague"
-    let queueName = "games"
 
-    let generateConnectionString() =
-        let connectionStringBuilder = new ServiceBusConnectionStringBuilder (ManagementPort = HttpPort, RuntimePort = TcpPort)
-        connectionStringBuilder.Endpoints.Add((new System.UriBuilder (Scheme = "sb", Host = _serverFqdn, Path = ServiceNamespace)).Uri) |> ignore
-        connectionStringBuilder.StsEndpoints.Add((new System.UriBuilder (Scheme = "https", Host = _serverFqdn, Port = HttpPort, Path = ServiceNamespace)).Uri) |> ignore
-        connectionStringBuilder.ToString()
-      
     override wr.Run() =
 
         log "RunsGames entry point called" "Information"
-        let messageFactory = MessagingFactory.CreateFromConnectionString(generateConnectionString());
-        let queueClient = messageFactory.CreateQueueClient(queueName)
-
         while(true) do 
+            Thread.Sleep(10000)
             log "Working" "Information"
-            // Receive the message from the queue 
-            let receivedMessage = queueClient.Receive(); 
-            if (receivedMessage <> null)  then
-                log ("Message received: " + receivedMessage.GetBody<String>()) "Information"
-                receivedMessage.Complete()
-            Thread.Sleep(5000)
-            
-
 
     override wr.OnStart() = 
 
@@ -53,9 +31,5 @@ type WorkerRole() =
        
         // For information on handling configuration changes
         // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
-
-        let namespaceManager = NamespaceManager.CreateFromConnectionString(generateConnectionString());
-        if (not(namespaceManager.QueueExists(queueName))) then
-            namespaceManager.CreateQueue(queueName) |> ignore
 
         base.OnStart()
